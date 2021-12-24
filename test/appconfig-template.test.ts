2a6657446dfdabb7da63f31991460dc326742f9e
Marcio Cruz de Almeida <marciocadev@gmail.com>
2021-12-24 13:42:25 -0300

    Initial commit

---
+++ test/appconfig-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppconfigTemplate from '../lib/appconfig-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appconfig-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppconfigTemplate.AppconfigTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
