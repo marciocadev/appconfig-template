4de1961d192ba721cc86d370462baf0acacb0557
Marcio Cruz de Almeida <marciocadev@gmail.com>
2021-12-24 14:10:34 -0300

    ajustes

--- lib/appconfig-template-stack.ts
+++ lib/appconfig-template-stack.ts
@@ -1,16 +1,109 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnResource, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnApplication, CfnConfigurationProfile, CfnDeployment, 
+  CfnDeploymentStrategy, CfnEnvironment, CfnHostedConfigurationVersion 
+} from 'aws-cdk-lib/aws-appconfig';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as devConfiguration from './configurations/dev.json';
+import * as hmgConfiguration from './configurations/hmg.json';
+import * as prdConfiguration from './configurations/prd.json';
+
 
 export class AppconfigTemplateStack extends Stack {
+  application:CfnResource;
+  confProfile:CfnResource;
+  deploymentStrategy:CfnResource
+
+  devEnv:CfnResource;
+  hmgEnv:CfnResource;
+  prdEnv:CfnResource;
+
+  devHostedConf:CfnResource;
+  hmgHostedConf:CfnResource;
+  prdHostedConf:CfnResource;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.application = new CfnApplication(this, 'BasicApplication', {
+      name: 'basic-application',
+      description: 'My basic AppConfig application'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppconfigTemplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.confProfile = new CfnConfigurationProfile(this, 'ConfigurationProfile', {
+      name: 'basic-configuration-profile',
+      applicationId: this.application.ref,
+      locationUri: 'hosted',
+      description: 'My basic AppConfig configuration profile example'
+    });
+
+    this.deploymentStrategy = new CfnDeploymentStrategy(this, 'DeploymentStrategy', {
+      name: 'basic-deployment-strategy',
+      deploymentDurationInMinutes: 0,
+      growthFactor: 100,
+      finalBakeTimeInMinutes: 0,
+      replicateTo: 'NONE',
+      growthType: 'LINEAR',
+      description: 'My basic AppConfig deployment strategy example'
+    });
+
+    // Create environments (DEV, HMG and PRD)
+    this.devEnv = new CfnEnvironment(this, 'devConfiguration', {
+      name: 'dev', 
+      applicationId: this.application.ref
+    });
+    this.hmgEnv = new CfnEnvironment(this, 'hmgConfiguration', {
+      name: 'hmg',
+      applicationId: this.application.ref
+    });
+    this.prdEnv = new CfnEnvironment(this, 'prdConfiguration', {
+      name: 'prd',
+      applicationId: this.application.ref
+    });
+
+    // Create hosted configuration for each environment
+    this.devHostedConf = new CfnHostedConfigurationVersion(this, 'DevHostedConfig', {
+      applicationId: this.application.ref,
+      configurationProfileId: this.confProfile.ref,
+      contentType: 'application/json',
+      content: JSON.stringify(devConfiguration),
+      description: 'My basic AppConfig dev hosted configuration version example'
+    });
+    this.hmgHostedConf = new CfnHostedConfigurationVersion(this, 'HmgHostedConfig', {
+      applicationId: this.application.ref,
+      configurationProfileId: this.confProfile.ref,
+      contentType: 'application/json',
+      content: JSON.stringify(hmgConfiguration),
+      description: 'My basic AppConfig hmg hosted configuration version example'
+    });
+    this.prdHostedConf = new CfnHostedConfigurationVersion(this, 'PrdHostedConfig', {
+      applicationId: this.application.ref,
+      configurationProfileId: this.confProfile.ref,
+      contentType: 'application/json',
+      content: JSON.stringify(prdConfiguration),
+      description: 'My basic AppConfig prd hosted configuration version example'
+    });
+
+    // Deploy environments
+    new CfnDeployment(this, 'DevDeployment', {
+      applicationId: this.application.ref,
+      configurationProfileId: this.confProfile.ref,
+      configurationVersion: this.devHostedConf.ref,
+      deploymentStrategyId: this.deploymentStrategy.ref,
+      environmentId: this.devEnv.ref
+    });
+    new CfnDeployment(this, 'HmgDeployment', {
+      applicationId: this.application.ref,
+      configurationProfileId: this.confProfile.ref,
+      configurationVersion: this.hmgHostedConf.ref,
+      deploymentStrategyId: this.deploymentStrategy.ref,
+      environmentId: this.hmgEnv.ref
+    });
+    new CfnDeployment(this, 'PrdDeployment', {
+      applicationId: this.application.ref,
+      configurationProfileId: this.confProfile.ref,
+      configurationVersion: this.prdHostedConf.ref,
+      deploymentStrategyId: this.deploymentStrategy.ref,
+      environmentId: this.prdEnv.ref
+    });
   }
 }
--- test/appconfig-template.test.ts
+++ test/appconfig-template.test.ts
@@ -1,17 +1,216 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AppconfigTemplate from '../lib/appconfig-template-stack';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { AppconfigTemplateStack } from '../lib/appconfig-template-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/appconfig-template-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AppconfigTemplate.AppconfigTemplateStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe('AppConfig Template Tests', () => {
+    let app:App;
+    let stack:AppconfigTemplateStack;
+    let template:Template;
+
+    let applicationId:string;
+    let configProfileId:string;
+    let deploymentStrategyId:string;
+
+    let devHostedConfigId:string;
+    let hmgHostedConfigId:string;
+    let prdHostedConfigId:string;
+
+    let devEnvId:string;
+    let hmgEnvId:string;
+    let prdEnvId:string;
+
+    beforeAll(() => {
+        // GIVEN
+        app = new App();
+        // WHEN
+        stack = new AppconfigTemplateStack(app, 'MyAppConfigTemplateTest');
+        // THEN
+        template = Template.fromStack(stack);
+
+        applicationId = stack.resolve(stack.application.node.id);
+        configProfileId = stack.resolve(stack.confProfile.node.id);
+        deploymentStrategyId = stack.resolve(stack.deploymentStrategy.node.id);
+
+        devHostedConfigId = stack.resolve(stack.devHostedConf.node.id);
+        hmgHostedConfigId = stack.resolve(stack.hmgHostedConf.node.id);
+        prdHostedConfigId = stack.resolve(stack.prdHostedConf.node.id);
+
+        devEnvId = stack.resolve(stack.devEnv.node.id);
+        hmgEnvId = stack.resolve(stack.hmgEnv.node.id);
+        prdEnvId = stack.resolve(stack.prdEnv.node.id);
+    });
+
+    test(('Snapshot validation'), () => {
+        expect(template).toMatchSnapshot();
+    });
+
+    test(('AppConfig application created'), () => {
+        template.resourceCountIs('AWS::AppConfig::Application', 1);
+        template.hasResourceProperties('AWS::AppConfig::Application', {
+            Name: 'basic-application'
+        });
+    });
+
+    test('AppConfig Configuration Profile Created', () => {
+        template.resourceCountIs('AWS::AppConfig::ConfigurationProfile', 1);
+        template.hasResourceProperties('AWS::AppConfig::ConfigurationProfile', {
+            Name: 'basic-configuration-profile',
+            LocationUri: 'hosted',
+            ApplicationId: {
+                Ref: applicationId
+            }
+        });
+    });
+
+    test('AppConfig Deployment Strategy Created', () => {
+        template.resourceCountIs('AWS::AppConfig::DeploymentStrategy', 1);
+        template.hasResourceProperties('AWS::AppConfig::DeploymentStrategy', {
+            Name: 'basic-deployment-strategy',
+            DeploymentDurationInMinutes: 0,
+            GrowthFactor: 100,
+            FinalBakeTimeInMinutes: 0,
+            ReplicateTo: 'NONE',
+            GrowthType: 'LINEAR'
+        });
+    });
+
+    test('AppConfig Environment count', () => {
+        template.resourceCountIs('AWS::AppConfig::Environment', 3);
+    });
+
+    test('AppConfig DEV Environment Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::Environment', {
+            Name: 'dev',
+            ApplicationId: {
+                Ref: applicationId
+            }
+        });
+    });
+
+    test('AppConfig HMG Environment Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::Environment', {
+            Name: 'hmg',
+            ApplicationId: {
+                Ref: applicationId
+            }
+        });
+    });
+
+    test('AppConfig PRD Environment Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::Environment', {
+            Name: 'prd',
+            ApplicationId: {
+                Ref: applicationId
+            }
+        });
+    });
+
+    test('AppConfig Hosted Configuration Version count', () => {
+        template.resourceCountIs('AWS::AppConfig::HostedConfigurationVersion', 3);
+    });
+
+    test('AppConfig DEV Hosted Configuration Version Created', () => {
+        template.findResources('AWS::AppConfig::HostedConfigurationVersion');
+        template.hasResourceProperties('AWS::AppConfig::HostedConfigurationVersion', {
+            ApplicationId: {
+                Ref: applicationId
+            },
+            ConfigurationProfileId: {
+                Ref: configProfileId
+            },
+            ContentType: 'application/json',
+            Content: JSON.stringify({env: 'dev'})
+        });
+    });
+
+    test('AppConfig HMG Hosted Configuration Version Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::HostedConfigurationVersion', {
+            ApplicationId: {
+                Ref: applicationId
+            },
+            ConfigurationProfileId: {
+                Ref: configProfileId
+            },
+            ContentType: 'application/json',
+            Content: JSON.stringify({env: 'hmg'})
+        });
+    });
+
+    test('AppConfig PRD Hosted Configuration Version Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::HostedConfigurationVersion', {
+            ApplicationId: {
+                Ref: applicationId
+            },
+            ConfigurationProfileId: {
+                Ref: configProfileId
+            },
+            ContentType: 'application/json',
+            Content: JSON.stringify({env: 'prd'})
+        });
+    });
+
+    test('AppConfig Deployment count', () => {
+        template.resourceCountIs('AWS::AppConfig::Deployment', 3);
+    });
+
+    test('AppConfig DEV Deployment Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::Deployment', {
+            ApplicationId: {
+                Ref: applicationId
+            },
+            ConfigurationProfileId: {
+                Ref: configProfileId
+            },
+            ConfigurationVersion: {
+                Ref: devHostedConfigId
+            },
+            DeploymentStrategyId: {
+                Ref: deploymentStrategyId
+            },
+            EnvironmentId: {
+                Ref: devEnvId
+            }
+        });
+    });
+
+    test('AppConfig HMG Deployment Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::Deployment', {
+            ApplicationId: {
+                Ref: applicationId
+            },
+            ConfigurationProfileId: {
+                Ref: configProfileId
+            },
+            ConfigurationVersion: {
+                Ref: hmgHostedConfigId
+            },
+            DeploymentStrategyId: {
+                Ref: deploymentStrategyId
+            },
+            EnvironmentId: {
+                Ref: hmgEnvId
+            }
+        });
+    });
+
+    test('AppConfig PRD Deployment Created', () => {
+        template.hasResourceProperties('AWS::AppConfig::Deployment', {
+            ApplicationId: {
+                Ref: applicationId
+            },
+            ConfigurationProfileId: {
+                Ref: configProfileId
+            },
+            ConfigurationVersion: {
+                Ref: prdHostedConfigId
+            },
+            DeploymentStrategyId: {
+                Ref: deploymentStrategyId
+            },
+            EnvironmentId: {
+                Ref: prdEnvId
+            }
+        });
+    });
 });
